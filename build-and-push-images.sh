0de9299d25845b6db58396298ca97b2c3d854882
David Denton <denton.david@gmail.com>
2021-04-09 13:24:01 +0100

    add new stages to build image

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 docker login
+pushd amazonlinux-graal-ce-java11-lambda-runtime
 docker build -t http4k/amazonlinux-graal-ce-java11-lambda-runtime \
 -t http4k/amazonlinux-graal-ce-java11-lambda-runtime:amazonlinux2018.03.0.20191014.0-graalce20.1.0 \
 .
 docker push http4k/amazonlinux-graal-ce-java11-lambda-runtime
+popd
