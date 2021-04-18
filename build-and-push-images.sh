12349ea26b42fd811317495e15eafb9c5f5e7e66
David Denton <denton.david@gmail.com>
2021-04-18 10:55:01 +0100

    rename image

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -euo pipefail
 docker login
+
+pushd amazoncorretto-lambda-runtime
+./build-and-push.sh
+popd
+
 pushd amazonlinux-java-graal-ce-lambda-runtime
 ./build-and-push.sh
 popd
-pushd amazonlinux-java-lambda-runtime
-./build-and-push.sh
-popd
+
