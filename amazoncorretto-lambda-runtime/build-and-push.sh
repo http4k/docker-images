e7bc4a7248433fcc3dd4ce08694521672a627ba3
David Denton <denton.david@gmail.com>
2021-04-18 11:45:39 +0100

    reorg tags

--- amazoncorretto-lambda-runtime/build-and-push.sh
+++ amazoncorretto-lambda-runtime/build-and-push.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -euo pipefail
 export AMAZON_CORRETTO_VERSION=11
-docker build -t http4k/amazoncorretto-lambda-runtime \
---build-arg AMAZON_CORRETTO_VERSION=$AMAZON_CORRETTO_VERSION \
+docker build --build-arg AMAZON_CORRETTO_VERSION=$AMAZON_CORRETTO_VERSION \
+-t http4k/amazoncorretto-lambda-runtime \
 -t http4k/amazoncorretto-lambda-runtime:amazoncorretto$AMAZON_CORRETTO_VERSION \
 .
 docker push http4k/amazoncorretto-lambda-runtime
--- amazonlinux-java-graal-ce-lambda-runtime/build-and-push.sh
+++ amazonlinux-java-graal-ce-lambda-runtime/build-and-push.sh
@@ -4,10 +4,10 @@ export AWS_LINUX_VERSION=2018.03.0.20191014.0
 export JAVA_VERSION=java11
 export GRAAL_VERSION=21.0.0.2
 
-docker build -t http4k/amazonlinux-graal-ce-java-lambda-runtime \
---build-arg AWS_LINUX_VERSION=$AWS_LINUX_VERSION \
+docker build --build-arg AWS_LINUX_VERSION=$AWS_LINUX_VERSION \
 --build-arg JAVA_VERSION=$JAVA_VERSION \
 --build-arg GRAAL_VERSION=$GRAAL_VERSION \
+-t http4k/amazonlinux-graal-ce-java-lambda-runtime \
 -t http4k/amazonlinux-java-graal-ce-lambda-runtime:amazonlinux$AWS_LINUX_VERSION-$JAVA_VERSION-graalce$GRAAL_VERSION \
 .
 docker push http4k/amazonlinux-java-graal-ce-lambda-runtime
