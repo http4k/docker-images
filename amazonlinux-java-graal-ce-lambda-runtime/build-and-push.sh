69c394b14936bc862980d091960fc241052f0dfc
David Denton <denton.david@gmail.com>
2021-04-18 12:06:26 +0100

    fix tag

--- amazonlinux-java-graal-ce-lambda-runtime/build-and-push.sh
+++ amazonlinux-java-graal-ce-lambda-runtime/build-and-push.sh
@@ -7,7 +7,7 @@ export GRAAL_VERSION=21.0.0.2
 docker build --build-arg AWS_LINUX_VERSION=$AWS_LINUX_VERSION \
 --build-arg JAVA_VERSION=$JAVA_VERSION \
 --build-arg GRAAL_VERSION=$GRAAL_VERSION \
--t http4k/amazonlinux-graal-ce-java-lambda-runtime \
+-t http4k/amazonlinux-java-graal-ce-lambda-runtime \
 -t http4k/amazonlinux-java-graal-ce-lambda-runtime:amazonlinux$AWS_LINUX_VERSION-$JAVA_VERSION-graalce$GRAAL_VERSION \
 .
 docker push http4k/amazonlinux-java-graal-ce-lambda-runtime
