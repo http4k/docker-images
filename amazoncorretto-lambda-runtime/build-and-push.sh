7ed82a5fc74bf0af54f67082adb60858c4553ddd
David Denton <denton.david@gmail.com>
2021-05-14 12:11:14 +0100

    readd latest

--- amazoncorretto-lambda-runtime/build-and-push.sh
+++ amazoncorretto-lambda-runtime/build-and-push.sh
@@ -2,6 +2,7 @@
 set -eu
 export AMAZON_CORRETTO_VERSION=11
 docker build --build-arg AMAZON_CORRETTO_VERSION=$AMAZON_CORRETTO_VERSION \
+-t http4k/amazoncorretto-lambda-runtime:latest \
 -t http4k/amazoncorretto-lambda-runtime:amazoncorretto$AMAZON_CORRETTO_VERSION \
 .
 docker push http4k/amazoncorretto-lambda-runtime:amazoncorretto$AMAZON_CORRETTO_VERSION
--- amazonlinux-java-graal-ce-lambda-runtime/build-and-push.sh
+++ amazonlinux-java-graal-ce-lambda-runtime/build-and-push.sh
@@ -7,6 +7,7 @@ export GRAAL_VERSION=21.1.0
 docker build --build-arg AWS_LINUX_VERSION=$AWS_LINUX_VERSION \
 --build-arg JAVA_VERSION=$JAVA_VERSION \
 --build-arg GRAAL_VERSION=$GRAAL_VERSION \
+-t http4k/amazonlinux-java-graal-ce-lambda-runtime:latest \
 -t http4k/amazonlinux-java-graal-ce-lambda-runtime:amazonlinux$AWS_LINUX_VERSION-$JAVA_VERSION-graalce$GRAAL_VERSION \
 .
 docker push http4k/amazonlinux-java-graal-ce-lambda-runtime:amazonlinux$AWS_LINUX_VERSION-$JAVA_VERSION-graalce$GRAAL_VERSION
