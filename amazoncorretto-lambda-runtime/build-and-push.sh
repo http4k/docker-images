3c76081aa4709c4970935164b34f798cc944c2fc
David Denton <denton.david@gmail.com>
2021-05-14 11:32:32 +0100

    remove latest

--- amazoncorretto-lambda-runtime/build-and-push.sh
+++ amazoncorretto-lambda-runtime/build-and-push.sh
@@ -2,7 +2,6 @@
 set -eu
 export AMAZON_CORRETTO_VERSION=11
 docker build --build-arg AMAZON_CORRETTO_VERSION=$AMAZON_CORRETTO_VERSION \
--t http4k/amazoncorretto-lambda-runtime \
 -t http4k/amazoncorretto-lambda-runtime:amazoncorretto$AMAZON_CORRETTO_VERSION \
 .
 docker push http4k/amazoncorretto-lambda-runtime:amazoncorretto$AMAZON_CORRETTO_VERSION
