ab8675d5eef610eccbfab25dabcb3b653d34861c
David Denton <denton.david@gmail.com>
2021-05-29 11:01:05 +0100

    add java.net.http to corretto

--- amazoncorretto-lambda-runtime/build-and-push.sh
+++ amazoncorretto-lambda-runtime/build-and-push.sh
@@ -2,7 +2,8 @@
 set -eu
 export AMAZON_CORRETTO_VERSION=11
 docker build --build-arg AMAZON_CORRETTO_VERSION=$AMAZON_CORRETTO_VERSION \
+-t http4k/amazoncorretto-lambda-runtime \
 -t http4k/amazoncorretto-lambda-runtime:latest \
 -t http4k/amazoncorretto-lambda-runtime:amazoncorretto$AMAZON_CORRETTO_VERSION \
 .
-docker push http4k/amazoncorretto-lambda-runtime:amazoncorretto$AMAZON_CORRETTO_VERSION
+docker push -a http4k/amazoncorretto-lambda-runtime
