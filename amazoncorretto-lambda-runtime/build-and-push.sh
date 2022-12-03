2fea5d2caecee51a672f6d82c7774e24e5c4db55
David Denton <denton.david@gmail.com>
2022-12-03 09:29:38 +0000

    add java 17 and 19 images as they are now supported by corretto

--- amazoncorretto-lambda-runtime/build-and-push.sh
+++ amazoncorretto-lambda-runtime/build-and-push.sh
@@ -6,4 +6,19 @@ docker build --build-arg AMAZON_CORRETTO_VERSION=$AMAZON_CORRETTO_VERSION \
 -t http4k/amazoncorretto-lambda-runtime:latest \
 -t http4k/amazoncorretto-lambda-runtime:amazoncorretto$AMAZON_CORRETTO_VERSION \
 .
+
+export AMAZON_CORRETTO_VERSION=17
+docker build --build-arg AMAZON_CORRETTO_VERSION=$AMAZON_CORRETTO_VERSION \
+-t http4k/amazoncorretto-lambda-runtime \
+-t http4k/amazoncorretto-lambda-runtime:latest \
+-t http4k/amazoncorretto-lambda-runtime:amazoncorretto$AMAZON_CORRETTO_VERSION \
+.
+
+export AMAZON_CORRETTO_VERSION=19
+docker build --build-arg AMAZON_CORRETTO_VERSION=$AMAZON_CORRETTO_VERSION \
+-t http4k/amazoncorretto-lambda-runtime \
+-t http4k/amazoncorretto-lambda-runtime:latest \
+-t http4k/amazoncorretto-lambda-runtime:amazoncorretto$AMAZON_CORRETTO_VERSION \
+.
+
 docker push -a http4k/amazoncorretto-lambda-runtime
