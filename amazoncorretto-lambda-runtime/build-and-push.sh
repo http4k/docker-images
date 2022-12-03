4c280e668c4a29103f8fc16f309cc852e556dceb
David Denton <denton.david@gmail.com>
2022-12-03 09:32:26 +0000

    revert mistaken change to build file

--- amazoncorretto-lambda-runtime/build-and-push.sh
+++ amazoncorretto-lambda-runtime/build-and-push.sh
@@ -7,18 +7,4 @@ docker build --build-arg AMAZON_CORRETTO_VERSION=$AMAZON_CORRETTO_VERSION \
 -t http4k/amazoncorretto-lambda-runtime:amazoncorretto$AMAZON_CORRETTO_VERSION \
 .
 
-export AMAZON_CORRETTO_VERSION=17
-docker build --build-arg AMAZON_CORRETTO_VERSION=$AMAZON_CORRETTO_VERSION \
--t http4k/amazoncorretto-lambda-runtime \
--t http4k/amazoncorretto-lambda-runtime:latest \
--t http4k/amazoncorretto-lambda-runtime:amazoncorretto$AMAZON_CORRETTO_VERSION \
-.
-
-export AMAZON_CORRETTO_VERSION=19
-docker build --build-arg AMAZON_CORRETTO_VERSION=$AMAZON_CORRETTO_VERSION \
--t http4k/amazoncorretto-lambda-runtime \
--t http4k/amazoncorretto-lambda-runtime:latest \
--t http4k/amazoncorretto-lambda-runtime:amazoncorretto$AMAZON_CORRETTO_VERSION \
-.
-
 docker push -a http4k/amazoncorretto-lambda-runtime
