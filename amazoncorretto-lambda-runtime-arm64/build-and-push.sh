6b941b5fcc0574719c31f91e25e4342df95554d0
David <denton.david@gmail.com>
2023-02-04 17:18:25 +0000

    switch amd64 -> arm64

--- amazoncorretto-lambda-runtime-arm64/build-and-push.sh
+++ amazoncorretto-lambda-runtime-arm64/build-and-push.sh
@@ -3,11 +3,11 @@ set -eu
 export AMAZON_CORRETTO_VERSION=$1
 export MAJOR_VERSION="${AMAZON_CORRETTO_VERSION%%.*}"
 
-docker build --build-arg AMAZON_CORRETTO_IMAGE=arm64v8/amazoncorretto:$AMAZON_CORRETTO_VERSION \
--t http4k/amazoncorretto-lambda-runtime-arm64v8 \
--t http4k/amazoncorretto-lambda-runtime-arm64v8:latest \
--t http4k/amazoncorretto-lambda-runtime-arm64v8:amazoncorretto$MAJOR_VERSION \
--t http4k/amazoncorretto-lambda-runtime-arm64v8:amazoncorretto$AMAZON_CORRETTO_VERSION \
+docker build --platform linux/arm64 --build-arg AMAZON_CORRETTO_IMAGE=arm64v8/amazoncorretto:$AMAZON_CORRETTO_VERSION \
+-t http4k/amazoncorretto-lambda-runtime-arm64 \
+-t http4k/amazoncorretto-lambda-runtime-arm64:latest \
+-t http4k/amazoncorretto-lambda-runtime-arm64:amazoncorretto$MAJOR_VERSION \
+-t http4k/amazoncorretto-lambda-runtime-arm64:amazoncorretto$AMAZON_CORRETTO_VERSION \
 .
 
 docker push -a http4k/amazoncorretto-lambda-runtime-arm64v8
