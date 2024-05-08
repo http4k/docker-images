ca80fbf32c9caf92cf3f60bbd9f2bb5c81410cb3
David Denton <denton.david@gmail.com>
2024-05-08 09:20:29 +0100

    Update versions of docker images

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -2,7 +2,7 @@
 set -eu
 docker login
 
-JAVA_VERSIONS=("11.0.18" "17.0.6" "19.0.2" "21.0.2" "22.0.1")
+JAVA_VERSIONS=("11.0.18" "17.0.6" "19.0.2" "21.0.3")
 
 cd amazoncorretto-lambda-runtime
 for java_version in "${JAVA_VERSIONS[@]}"; do
@@ -16,7 +16,7 @@ for java_version in "${JAVA_VERSIONS[@]}"; do
 done
 cd ..
 
-GRAAL_JAVA_VERSIONS=("21.0.0")
+GRAAL_JAVA_VERSIONS=("21.0.0" "22.0.1")
 AWS_LINUX_VERSIONS=("2023.4.20240416.0" "2.0.20240412.0")
 
 cd amazonlinux-java-graal-community-lambda-runtime
