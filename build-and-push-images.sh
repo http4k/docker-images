41756cdee8efb873423408fb6d929368c994951d
David Denton <denton.david@gmail.com>
2024-05-08 09:11:32 +0100

    Add more images for graalvm images

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -2,7 +2,7 @@
 set -eu
 docker login
 
-JAVA_VERSIONS=("11.0.18" "17.0.6" "19.0.2" "21.0.0")
+JAVA_VERSIONS=("11.0.18" "17.0.6" "19.0.2" "21.0.2" "22.0.1")
 
 cd amazoncorretto-lambda-runtime
 for java_version in "${JAVA_VERSIONS[@]}"; do
@@ -17,7 +17,7 @@ done
 cd ..
 
 GRAAL_JAVA_VERSIONS=("21.0.0")
-AWS_LINUX_VERSIONS=("2.0.20240412.0", "2023.4.20240416.0")
+AWS_LINUX_VERSIONS=("2023.4.20240416.0" "2.0.20240412.0")
 
 cd amazonlinux-java-graal-community-lambda-runtime
 for java_version in "${GRAAL_JAVA_VERSIONS[@]}"; do
