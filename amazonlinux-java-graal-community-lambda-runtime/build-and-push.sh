4f6c324557e78b12de83d33b20d50e1a6309a128
David Denton <denton.david@gmail.com>
2024-05-08 09:05:02 +0100

    Update image versions for graalvm

--- amazonlinux-java-graal-community-lambda-runtime/build-and-push.sh
+++ amazonlinux-java-graal-community-lambda-runtime/build-and-push.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 set -eu
-export AWS_LINUX_VERSION=2.0.20230912.0
 export JAVA_VERSION=jdk-$1
+export AWS_LINUX_VERSION=$2
 
 docker build --platform linux/arm64 --build-arg AWS_LINUX_VERSION=$AWS_LINUX_VERSION \
 --build-arg ARCH=aarch64 \
 --build-arg JAVA_VERSION="$JAVA_VERSION" \
 -t http4k/amazonlinux-java-graal-community-lambda-runtime:latest-arm64 \
 -t http4k/amazonlinux-java-graal-community-lambda-runtime:amazonlinux$AWS_LINUX_VERSION-"$JAVA_VERSION"-arm64 .
-
 
 docker build --build-arg AWS_LINUX_VERSION=$AWS_LINUX_VERSION \
 --build-arg ARCH=x64 \
--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -5,21 +5,24 @@ docker login
 JAVA_VERSIONS=("11.0.18" "17.0.6" "19.0.2" "21.0.0")
 
 cd amazoncorretto-lambda-runtime
-for version in "${JAVA_VERSIONS[@]}"; do
-    ./build-and-push.sh "$version"
+for java_version in "${JAVA_VERSIONS[@]}"; do
+    ./build-and-push.sh "$java_version"
 done
 cd ..
 
 cd amazoncorretto-lambda-runtime-arm64
-for version in "${JAVA_VERSIONS[@]}"; do
-    ./build-and-push.sh "$version"
+for java_version in "${JAVA_VERSIONS[@]}"; do
+    ./build-and-push.sh "$java_version"
 done
 cd ..
 
 GRAAL_JAVA_VERSIONS=("21.0.0")
+AWS_LINUX_VERSIONS=("2.0.20240412.0", "2023.4.20240416.0")
 
 cd amazonlinux-java-graal-community-lambda-runtime
-for version in "${GRAAL_JAVA_VERSIONS[@]}"; do
-    ./build-and-push.sh "$version"
+for java_version in "${GRAAL_JAVA_VERSIONS[@]}"; do
+  for linux_version in "${AWS_LINUX_VERSIONS[@]}"; do
+    ./build-and-push.sh "$java_version" "$linux_version"
+  done
 done
 cd ..
